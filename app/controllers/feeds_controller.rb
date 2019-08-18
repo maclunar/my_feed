344e6796a29f77898f98665a8665df53379f2c0c
Maciej Wilk <maclunar@gmail.com>
2019-08-18 15:08:13 +0200

    use sources checkboxes

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -2,19 +2,12 @@ class FeedsController < ApplicationController
   def new; end
 
   def show
-    @feeds = FeedGetter.new(feed_params).feeds
+    @feeds = FeedGetter.new(feed_params.to_h).feeds
   end
 
   private
 
-  def permitted_params
-    params.permit(:tag, sources: [:twitter, :stackoverflow, :youtube])
-  end
-
   def feed_params
-    {
-      tag: permitted_params[:tag],
-      sources: permitted_params[:sources].to_h.keys
-    }
+    params.require(:feed).permit(:tag, sources: [:twitter, :stackoverflow, :youtube])
   end
 end
--- app/services/feed_getter.rb
+++ app/services/feed_getter.rb
@@ -1,17 +1,28 @@
 class FeedGetter
   def initialize(params)
     @tag = params[:tag]
+    @sources = selected_sources(params[:sources])
   end
 
   def feeds
-    {
-      'stackoverflow': StackoverflowConnection.new(tag).feed,
-      'twitter': TwitterConnection.new(tag).feed,
-      'youtube': YoutubeConnection.new(tag).feed
-    }
+    retrieved_feeds = {}
+
+    sources.each do |source|
+      retrieved_feeds[source] = retrieve_feed(source)
+    end
+
+    retrieved_feeds
   end
 
   private
 
-  attr_reader :tag
+  attr_reader :tag, :sources
+
+  def selected_sources(sources_hash)
+    sources_hash.select { |_, v| v == '1' }.keys
+  end
+
+  def retrieve_feed(source)
+    "#{source.capitalize}Connection".constantize.new(tag).feed
+  end
 end
